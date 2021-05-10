2ed3da2b5af735456c4fab628da6a74b6a7630fd
Amey Aditya Achar J <ameyaditya.j@gmail.com>
2021-05-10 18:34:09 +0530

    made small changes to the code

--- Car Controller/main.py
+++ Car Controller/main.py
@@ -79,4 +79,4 @@ def kill_switch():
         return generate_response({"message": "ERROR OCCURED", "error": e}, 500)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0")
