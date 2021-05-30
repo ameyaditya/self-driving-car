0c0388a53fd570d7e87d8f58ef2d85c8a118d71d
Amey Aditya Achar J <ameyaditya.j@gmail.com>
2021-05-30 20:29:53 +0530

    added new streamign code

--- Pi Camera Streaming/main.py
+++ Pi Camera Streaming/main.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 import time
 import threading
 import os
-
+import io
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import cv2
@@ -14,23 +14,31 @@ camera.framerate = 10
 rawCapture = PiRGBArray(camera, size=(736,480))
 camera.capture(rawCapture, format="bgr")
 print("INITIALISED THE CAMERA")
+my_stream = io.BytesIO()
 
-def get_frame():
-    image = rawCapture.array
-    ret, jpeg = cv2.imencode('.jpg', image)
-    return jpeg.tobytes()
+
+# def get_frame():
+#     image = camera.capture(format="jpeg", use_video_port=True)
+    
+#     return jpeg.tobytes()
 
 # pi_camera = VideoCamera(flip=False)
-
+# with PiCamera() as camera:
+#     camera.start_preview()
+#     time.sleep(2)
+#     with PiRGBArray(camera) as stream:
+#         camera.capture(stream, format='bgr')
+#         # At this point the image is available as stream.array
+#         image = stream.array
+    
 app = Flask(__name__)
 CORS(app)
 
-def gen():
-    while True:
-        print("GETTING A FRAME")
-        frame = get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+# def gen():
+#     while True:
+#         frame = get_frame()
+#         yield (b'--frame\r\n'
+#                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 
 @app.route('/video_feed')
---
+++ Pi Camera Streaming/main2.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, Response
+from flask_cors import CORS
+import picamera
+import cv2
+import socket
+import io
+
+app = Flask(__name__)
+CORS(app)
+vc = cv2.VideoCapture(0)
+
+def gen():
+    while True:
+        rval, frame = vc.read()
+        cv2.imwrite('pic.jpg', frame)
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' +
+               open('pic.jpg', 'rb').read()
+               + b'\r\n')
+
+
+@app.route('/video_feed') 
+def video_feed(): 
+   return Response(gen(), 
+                   mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__': 
+	app.run(host='0.0.0.0', port=5001, threaded=True) 
