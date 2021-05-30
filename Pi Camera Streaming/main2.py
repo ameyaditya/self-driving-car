252bc7d4e353274bbfb454db7179ab6dd8dfdfbc
Amey Aditya Achar J <ameyaditya.j@gmail.com>
2021-05-30 20:42:05 +0530

    made changes from writing to file, to converting to bytes

--- Pi Camera Streaming/main2.py
+++ Pi Camera Streaming/main2.py
@@ -13,12 +13,14 @@ def gen():
     while True:
         rval, frame = vc.read()
         print(type(frame))
+        success, encoded_image = cv2.imencode('.jpg', image)
+        bytes_image = encoded_image.tobytes()
         cv2.imwrite('pic.jpg', frame)
         image = open('pic.jpg', 'rb').read()
         print(type(image))
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' +
-               image
+               bytes_image
                + b'\r\n')
 
 
