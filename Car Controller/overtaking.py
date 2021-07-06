710647f3cab8c76a5c56004bf20de8db7a6e8b17
anirudh_j98 <anirudh101298@gmail.com>
2021-07-07 03:45:35 +0530

    Made changes

---
+++ Car Controller/overtaking.py
@@ -0,0 +1,19 @@
+from car_rotation import CarRotation as cr
+import requests
+threshold = 30
+turn_time = 1
+overtake_time=5
+while True:
+    distance = requests.get("/api/vi/distance")
+
+    if distance <=30:
+        cr.turn_full_right()
+        time.sleep(turn_time)
+        cr.turn_center()
+        time.sleep(overtake_time)
+        cr.turn_full_left()
+        time.sleep(turn_time)
+        cr.turn_center()
+    
+
+
