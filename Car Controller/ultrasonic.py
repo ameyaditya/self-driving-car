e2b423907c1d3b2d93112a47aead29d3dfaa1660
anirudh_j98 <anirudh101298@gmail.com>
2021-07-07 03:57:24 +0530

    made changes

--- Car Controller/ultrasonic.py
+++ Car Controller/ultrasonic.py
@@ -7,6 +7,8 @@ class Ultrasonic:
         self.ECHO_PIN = echo_pin
 
     def initialise(self):
+        GPIO.setup(self.TRIGGER_PIN, GPIO.OUT)
+        GPIO.setup(self.ECHO_PIN, GPIO.IN)
         GPIO.output(self.TRIGGER_PIN, False)
         time.sleep(2)
     
@@ -15,10 +17,10 @@ class Ultrasonic:
         time.sleep(0.00001)
         GPIO.output(self.TRIGGER_PIN, False)
 
-        while GPIO.input(c.ECHO)==0:
+        while GPIO.input(self.ECHO_PIN)==0:
             pulse_start = time.time()
         
-        while GPIO.input(c.ECHO)==1:
+        while GPIO.input(self.ECHOs_PIN)==1:
             pulse_end = time.time()
         pulse_duration = pulse_end - pulse_start
 
