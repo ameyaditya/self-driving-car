0903d4d58cc3a198bab2f49c787a01b878cd760d
ameyaditya <ameyaditya.j@gmail.com>
2021-07-06 09:03:55 +0530

    made changes

--- test_codes/servo-reset.py
+++ test_codes/servo-reset.py
@@ -12,14 +12,15 @@ CONTROL_PIN.start(DUTY_CYCLE)
 time.sleep(3)
 
 CONTROL_PIN.ChangeDutyCycle(7.4)
+time.sleep(0.2)
 print("INITIAL TEST DONE")
 
 
-GPIO.output(SERVO_PIN, False)
-for i in range(5):
-    GPIO.output(SERVO_PIN, True)
-    CONTROL_PIN = GPIO.PWM(SERVO_PIN, SERVO_FREQUENCY)
-    CONTROL_PIN.ChangeDutyCycle(DUTY_CYCLE + 1)
+# GPIO.output(SERVO_PIN, False)
+# for i in range(5):
+#     GPIO.output(SERVO_PIN, True)
+#     CONTROL_PIN = GPIO.PWM(SERVO_PIN, SERVO_FREQUENCY)
+#     CONTROL_PIN.ChangeDutyCycle(DUTY_CYCLE + 1)
 input()
 
 
