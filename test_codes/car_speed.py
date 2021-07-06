201b677eceea278a0ab3adf9a82b2e9786dc3692
ameyaditya <ameyaditya.j@gmail.com>
2021-07-06 12:36:48 +0530

    made changes

--- test_codes/car_speed.py
+++ test_codes/car_speed.py
@@ -11,10 +11,7 @@ M1_CONTROLLER = GPIO.PWM(M1_F, 100)
 M1_CONTROLLER.start(0)
 time.sleep(1)
 
-M1_CONTROLLER.ChangeDutyCycle(10)
-time.sleep(5)
-
-M1_CONTROLLER.ChangeDutyCycle(0)
+M1_CONTROLLER.ChangeDutyCycle(30)
 time.sleep(5)
 
 M1_CONTROLLER.ChangeDutyCycle(100)
